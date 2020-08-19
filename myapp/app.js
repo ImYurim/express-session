6a4ed3f6a6c0312662ca02597888cd9bcd272c07
임유림 <ymyu124@naver.com>
2020-08-19 12:04:26 +0900

    session

--- myapp/app.js
+++ myapp/app.js
@@ -3,11 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { RequestHeaderFieldsTooLarge } = require('http-errors');
 
 var app = express();
+
+
+//session
+app.use(session({
+  secret: 'asdfaewfwe1231rsadf',
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore()
+}))
+
+app.get('/',function(req,res){
+  console.log(req.session);
+  if(req.session.num===undefined){
+    req.session.num=1;
+  }else{
+    req.session.num=req.session.num+1;
+  }
+  res.send(`View = ${req.session.num}`);
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +43,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
